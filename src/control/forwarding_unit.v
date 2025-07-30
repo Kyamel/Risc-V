5d288ee4d79eb44bb9d86e0391de81da0719f1cb
JoaoVitor <joaovitorshenriques@gmail.com>
2025-07-29 23:01:57 -0300

    feat: adicionando logica para a unidade de forward

--- src/control/forwarding_unit.v
+++ src/control/forwarding_unit.v
@@ -13,6 +13,44 @@ module forwarding_unit (
     input wire mem_wb_regwrite,
     
     // Outputs de seleção
-    output wire [1:0] forward_a,  // 00: ID/EX, 01: MEM/WB, 10: EX/MEM
+    output wire [1:0] forward_a,
     output wire [1:0] forward_b
 );
+
+/*    * Valores dos sinais de controle 'forward_a' e 'forward_b':
+    * 00: Sem adiantamento. A ULA usa o valor vindo do banco de registradores (estágio ID).
+    * 01: Adiantar do estágio EX/MEM. A ULA usa o resultado da instrução que está no estágio EX/MEM.
+    * 10: Adiantar do estágio MEM/WB. A ULA usa o resultado da instrução que está no estágio MEM/WB.
+*/
+
+
+always @(*) begin
+
+    // ---- Lógica para o A (rs1) ----
+
+    if(ex_mem_regwrite && (ex_mem_rd != 5'b00000) && (ex_mem_rd == id_ex_rs1))begin
+        forward_a = 2'b01; // adianta o estágio ex/mem
+    end
+
+    else if (mem_wb_regwrite && (mem_wb_rd != 5'b00000) && (mem_wb_rd == id_ex_rs1))begin
+        forward_a = 2'b10; // adianta o estágio mem/wb
+    end
+    else begin
+        forward_a = 2'b00; // sem adiantamento
+    end
+
+    // ---- Lógica para o B (rs2) ----
+
+        if(ex_mem_regwrite && (ex_mem_rd != 5'b00000) && (ex_mem_rd == id_ex_rs2))begin
+        forward_b = 2'b01; // adianta o estágio ex/mem
+    end
+
+    else if (mem_wb_regwrite && (mem_wb_rd != 5'b00000) && (mem_wb_rd == id_ex_rs2))begin
+        forward_b = 2'b10; // adianta o estágio mem/wb
+    end
+    else begin
+        forward_b = 2'b00; // sem adiantamento
+    end
+    
+end
+
