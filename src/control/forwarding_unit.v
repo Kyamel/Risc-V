6eb60a0224d700ea8fd6e35bab3616dc0ec1da09
JoaoVitor <joaovitorshenriques@gmail.com>
2025-07-29 22:47:53 -0300

    fix: ajustando modulo da unidade de forward

--- src/control/forwarding_unit.v
+++ src/control/forwarding_unit.v
@@ -1,10 +1,16 @@
 module forwarding_unit (
-    // Endereços de registradores
-    input wire [3:0] ex_rs1, ex_rs2,
-    input wire [3:0] mem_rd, wb_rd,
+
+    // Identificador do registrador vindo do estágio ID/EX
+    input wire [4:0] id_ex_rs1,
+    input wire [4:0] id_ex_rs2,
+
+    // Identificador do registrador de destino e sinal de escrita vindo do estágio EX/MEM
+    input wire [4:0] ex_mem_rd,
+    input wire ex_mem_regwrite,
     
-    // Sinais de controle
-    input wire mem_reg_write, wb_reg_write,
+    // Identificador do registrador de destino e sinal de escrita vindo do estágio MEM/WB
+    input wire [4:0] mem_wb_rd,
+    input wire mem_wb_regwrite,
     
     // Outputs de seleção
     output wire [1:0] forward_a,  // 00: ID/EX, 01: MEM/WB, 10: EX/MEM
